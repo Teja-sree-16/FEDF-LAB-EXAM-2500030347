c5acfb8cd74bf76f1dafec3f6e467a3126dd93c9
TejaSree <2500030347@kluniversity.in>
2026-03-13 11:07:41 +0530

    modified files

--- src/exam/ApiDemo.jsx
+++ src/exam/ApiDemo.jsx
@@ -1,26 +1,70 @@
-import axios from "axios";
-import { useEffect, useState } from "react";
+import axios from 'axios'
+import React, { useEffect, useState } from 'react'
 
-export default function APIdemo() {
-  const [data, setData] = useState([]);
+export default function Api() 
+{
+   const[data,setData] = useState([])
+   const[error,setError] = useState("")
 
-  useEffect(() => {
-    const fetchData = async () => {
-      let res = await axios.get("https://dummy-json.mock.beeceptor.com/posts");
-      setData(res.data);
-    };
+    const showData = async () =>{
 
-    fetchData();
-  }, []);
+        try
+         {
+            const response =  await axios.get("https://dummy-json.mock.beeceptor.com/posts")
+            setData(response.data)
+        } 
+        catch (error) 
+        {
+            setError(error.message)
+            
+        }
+    }
+ 
+    useEffect(() => {
+        
+         showData()
+    }, []);
+
 
   return (
-    <div className="container">
-      <h2>API Demo</h2>
-      <ul>
-        {data.map((u) => (
-          <li key={u.id}>{u.title} - {u.body}</li>
-        ))}
-      </ul>
+    <div>
+        <h2><u>Axios Demo</u></h2>
+         {
+            error?<b>{error}</b>:
+            data.length == 0?
+            <b>loading ...</b>:
+            <table border={1}>
+                <tr>
+                    <th>ID</th>
+                    <th>Title</th>
+                    <th>body</th>
+                    <th>link</th>
+                    <th>Category</th>
+                    
+                    
+                </tr>
+                {
+                   data.map((product,index)=>(
+                    <tr key={index}>
+                      <td>{product.id}</td>
+                      <td>{product.title}</td>
+                      <td>{product.body}</td>
+                      <td>{product.link}</td>
+                      <td>{product.category}</td>
+                      <td>
+                        <img src={product.image} width="30%" height="30%"/></td>
+                      
+                      
+                    </tr>
+
+
+                )) 
+                }
+            </table>
+         }
+
+
+
     </div>
-  );
+  )
 }
